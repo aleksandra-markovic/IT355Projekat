4db237fee0763add8fdee907602fe5e31c25ab24
aleksandra-markovic <aleksandra.markovic.6305@metropolitan.ac.rs>
2026-05-09 23:06:44 +0200

    Add hardcoded events and comments

--- events/src/main/java/com/eventmanager/events/service/CommentService.java
+++ events/src/main/java/com/eventmanager/events/service/CommentService.java
@@ -2,6 +2,7 @@ package com.eventmanager.events.service;
 
 import com.eventmanager.events.model.Comment;
 import com.eventmanager.events.model.Event;
+import com.eventmanager.events.model.User;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,8 +14,49 @@ public class CommentService {
 
     private List<Comment> comments;
 
-    public CommentService () {
+    private final EventService eventService;
+
+    public CommentService (EventService eventService) {
+        this.eventService = eventService;
+
         comments = new ArrayList<>();
+
+        User user1 = new User("Aleksandra", "aleks@gmail.com");
+        User user2 = new User("Anja", "anja@gmail.com");
+
+
+        Event e1 = eventService.getEventById(1L);
+        Event e2 = eventService.getEventById(2L);
+        Event e3 = eventService.getEventById(3L);
+
+        Comment c1 = new Comment(
+                "Jedva cekam ovaj dogadjaj!",
+                user2,
+                e1
+        );
+
+        Comment c2 = new Comment(
+                "Bio sam prosle godine, bilo je odlicno.",
+                user1,
+                e1
+        );
+
+        Comment c3 = new Comment(
+                "Dolazim sa ekipom!",
+                user1,
+                e2
+        );
+
+        Comment c4 = new Comment(
+                "Koji bendovi nastupaju?",
+                user2,
+                e3
+        );
+
+        comments.add(c1);
+        comments.add(c2);
+        comments.add(c3);
+        comments.add(c4);
     }
 
     public void addComment (Comment comment) {
--- events/src/main/java/com/eventmanager/events/service/EventService.java
+++ events/src/main/java/com/eventmanager/events/service/EventService.java
@@ -1,9 +1,12 @@
 package com.eventmanager.events.service;
 
+import com.eventmanager.events.model.Category;
 import com.eventmanager.events.model.Event;
+import com.eventmanager.events.model.Location;
 import com.eventmanager.events.model.User;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -16,7 +19,56 @@ public class EventService {
     private long idCounter = 0;
 
     public EventService () {
+
         events = new ArrayList<>();
+
+        User user1 = new User("Aleksandra", "aleks@gmail.com");
+        User user2 = new User("Marko", "marko@gmail.com");
+
+        Event e1 = new Event(
+                1L,
+                "Java konferencija",
+                LocalDateTime.of(2026, 6, 15, 18, 0),
+                Category.SEMINAR,
+                new Location("Beograd", "Knez Mihailova 10", "Sava Centar"),
+                user1,
+                new ArrayList<>()
+        );
+
+        Event e2 = new Event(
+                2L,
+                "Gaming turnir",
+                LocalDateTime.of(2026, 7, 2, 20, 0),
+                Category.OTHER,
+                new Location("Novi Sad", "Bulevar Oslobodjenja 25", "SPENS"),
+                user2,
+                new ArrayList<>()
+        );
+
+        Event e3 = new Event(
+                3L,
+                "Rock koncert",
+                LocalDateTime.of(2026, 8, 10, 21, 30),
+                Category.CONCERT,
+                new Location("Nis", "Generala Milojka Lesjanina 5", "Tvrdjava"),
+                user1,
+                new ArrayList<>()
+        );
+
+        Event e4 = new Event(
+                4L,
+                "Sajam knjiga",
+                LocalDateTime.of(2026, 9, 20, 12, 0),
+                Category.OTHER,
+                new Location("Beograd", "Bulevar Vojvode Misica", "Beogradski sajam"),
+                user2,
+                new ArrayList<>()
+        );
+
+        events.add(e1);
+        events.add(e2);
+        events.add(e3);
+        events.add(e4);
     }
 
     public void addEvent(Event event) {
